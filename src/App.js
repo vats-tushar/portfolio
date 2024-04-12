5ae97631d7bcaa7568f29ea9e687e810fbe3be38
vats-tushar <121571005+vats-tushar@users.noreply.github.com>
2024-04-12 09:43:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import MainPage from './components/MainPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MoreProjects from './components/MoreProjects';
-import VidEdits from './components/VidEdits';
 function App() {
 
   return (
@@ -11,7 +10,6 @@ function App() {
         <Routes>
           <Route exact path='/' element={<MainPage/>}></Route>
           <Route exact path='/moreprojects' element={<MoreProjects/>}></Route>
-          <Route exact path='/edits' element={<VidEdits/>}></Route>
         </Routes>
       </Router>
       
